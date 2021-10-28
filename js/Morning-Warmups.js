5805299c25200d0ed06cda3329de1457c23532ee
Everett-Stubbs <everettstubbs4@gmail.com>
2021-10-28 14:24:31 -0500

    pushing to github

---
+++ js/Morning-Warmups.js
@@ -0,0 +1,52 @@
+(function()) {
+
+        function returnsSeven() {
+            return7;
+        }
+
+        console.log(returnsSeven());
+}
+Write a function called 'typeCheck' that takes an input and returns a string of the data type entered.
+If the data type is a string, return the string "Haha, it's a string of string!"
+
+Tests:
+    typeCheck('')
+typeCheck(isNaN());
+typeCheck(!false * 3);
+typeCheck(4 + '4');
+
+function typeCheck() {
+    if (typeof input === 'string') {
+    }
+    return "haha, it\'s a string of a string"
+} else {
+
+    console.log('')
+}
+function secondToLast(arr) {
+    return arr[arr.length - 2];
+
+}
+
+console.log(secondToLast){"First", "Second", "Not Last", "Last"})();
+
+function filterForCanadians(arr) {
+    var bucket = [];
+    arr.forEach(function (element))
+    {
+        if (element.nationality === "Canadian") {
+            bucket.push(element)
+        }
+    });
+    return bucket;
+
+    {
+
+    console.log(filterForCanadians(wrestlers));
+
+
+    });
+
+
+
+})();
---
+++ js/planets-string.js
@@ -0,0 +1,41 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = planetsString.split(1)
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+
+    // console.log(planetsArray);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     *
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+
+        var planetsWithBreakTags = planetsArray.join(<br>);
+        console.log(planetsWithBreakTags);
+
+        var unorderedListOfPlanets = "<ul><li>" + planetsArray.join("</li><li") + ("</li><ul>")
+        console.log(unorderedListOfPlanets);
+
+        var listOfPlanets
+
+        planetsArray.unshift("<ul>");
+        planetsArray.push("</ul>");
+        listOfPlanets = planetsArray.join(" <li> ");
+
+        document.write(unorderedListOfPlanets);
+
+})();
