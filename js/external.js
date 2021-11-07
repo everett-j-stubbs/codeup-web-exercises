85633ef6aeffce2af3d2bd8a1e8c9002385e1df3
Everett-Stubbs <everettstubbs4@gmail.com>
2021-11-07 17:58:02 -0600

    completed exercises

--- js/external.js
+++ js/external.js
@@ -3,4 +3,4 @@ console.log("The user entered: " + "Hello! From my folder userInput");
 alert "Welcome to my Website"
 prompt "What is the user's favorite color?"
 alert "My favorite color is blue."
-alert Great, blue is my favorite color too!"
+alert "Great, blue is my favorite color too!"
